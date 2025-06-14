aad197881a051ccbbdc8edd056c21f9b8b0515c3
Ayushpanwar2002 <104716917+ayushpanwar112@users.noreply.github.com>
2025-06-14 23:36:40 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 // Middleware setup
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"], // example of multiple origins
+    origin: ["https://ubiquitous-panda-ba9e91.netlify.app", "https://endearing-bublanina-c2fc60.netlify.app"], // example of multiple origins
     credentials: true,
   })
 );
